0fcef22b473a28bec0a6b55adbff6bb76bb53c8f
lamnta <lamntaph14414@fpt.edu.vn>
2022-02-22 10:32:51 +0700

    up nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ const Nav = {
             <nav>
                 <ul class="menu">
                     <li class=menu-item><a href="/" class=menu-item_link>Trang chủ</li>
-                    <li class=menu-item><a href="/gioithieu" class=menu-item_link>Giới thiệu</li>
+                    <li class=menu-item><a href="/gioithieu" class=menu-item_link>Tin tức</li>
                     <li class=menu-item><a href="/product" class=menu-item_link>Sản phẩm</li>
                     <li class=menu-item><a href="/contact" class=menu-item_link>Liên hệ</li>
                     <li class=menu-item><a href="/admin/dashboard" class=menu-item_link>Admin</li>
