caa1d2bc948ee78f38f166e1bb1ed210ac6695af
lamnta <lamntaph14414@fpt.edu.vn>
2022-02-23 15:27:08 +0700

    add edit news

--- src/pages/admin/news/edit.js
+++ src/pages/admin/news/edit.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import $ from "jquery";
-import validate from "jquery-validation";
-import { add } from "../../../api/post";
+import { get, update } from "../../../api/post";
 import AdminNav from "../../../components/AdminNav";
 
-const AdminAddNews = {
-    render() {
+const AdminEditNews = {
+    async render(id) {
+        const { data } = await get(id);
         return /* html */ `
         <div class="min-h-full">
         ${AdminNav.render()}
@@ -16,11 +15,11 @@ const AdminAddNews = {
               <div class="lg:flex lg:items-center lg:justify-between">
                 <div class="flex-1 min-w-0">
                   <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-                    Thêm mới bài viết
+                    Cập nhật bài viết
                   </h2>
                 </div>
                 <div class="mt-5 flex lg:mt-0 lg:ml-4">
-                  <a href="/admin/news/add" class="sm:ml-3">
+                  <a href="/admin/news" class="sm:ml-3">
                     <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                       <svg xmlns="http://www.w3.org/2000/svg" class="-ml-1 mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
@@ -30,27 +29,32 @@ const AdminAddNews = {
                   </a>
                 </div>
               </div>
-      
           </div>
         </header>
         <main>
           <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <!-- Replace with your content -->
-            <form action="" id="form-add">
+            <form action="" id="form-edit">
               <input type="text"
                     id="title-post"
                     class="border border-black"
                     placeholder="Title"
-                    name="title-post"
+                    value="${data.title}"
               > <br />
               <input type="file"
                     id="img-post"
                     class="border border-black"
                     placeholder="Image"
               > <br />
-              <img src="http://2.bp.blogspot.com/-MowVHfLkoZU/VhgIRyPbIoI/AAAAAAAATtI/fHk-j_MYUBs/s640/placeholder-image.jpg" id="img-preview"/>
-              <textarea name="" id="desc-post" cols="30" rows="10" class="border border-black"></textarea><br />
-              <button class="bg-blue-500 p-4 text-white">Thêm</button>
+              <img src="${data.img}" id="img-preview"/>
+              <textarea name="" 
+                id="desc-post" 
+                cols="30" 
+                rows="10" 
+                class="border border-black"
+                value="${data.desc}"
+              ></textarea><br />
+              <button class="bg-blue-500 p-4 text-white">Cập nhật</button>
             </form>
             <!-- /End replace -->
           </div>
@@ -59,60 +63,44 @@ const AdminAddNews = {
         
         `;
     },
-    afterRender() {
-        const formAdd = $("#form-add");
+    afterRender(id) {
+        const formEdit = document.querySelector("#form-edit");
         const imgPost = document.querySelector("#img-post");
         const imgPreview = document.querySelector("#img-preview");
+        let imgLink = "";
 
         const CLOUDINARY_API = "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload";
         const CLOUDINARY_PRESET = "jkbdphzy";
-
-        let imgLink = "";
 
         // preview image when upload
         imgPost.addEventListener("change", async (e) => {
             imgPreview.src = URL.createObjectURL(e.target.files[0]);
         });
 
-        formAdd.validate({
-            rules: {
-                "title-post": {
-                    required: true,
-                    minlength: 5,
-                },
-            },
-            messages: {
-                "title-post": {
-                    required: "Không được để trống trường này!",
-                    minlength: "Nhập ít nhất 5 ký tự",
-                },
-            },
-            submitHandler() {
-                async function addProduct() {
-                    const file = imgPost.files[0];
-                    if (file) {
-                        const formData = new FormData();
-                        formData.append("file", file);
-                        formData.append("upload_preset", CLOUDINARY_PRESET);
+        formEdit.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            const file = imgPost.files[0];
+            if (file) {
+                const formData = new FormData();
+                formData.append("file", file);
+                formData.append("upload_preset", CLOUDINARY_PRESET);
 
-                        // call api cloudinary
+                // call api cloudinary
 
-                        const { data } = await axios.post(CLOUDINARY_API, formData, {
-                            headers: {
-                                "Content-Type": "application/form-data",
-                            },
-                        });
-                        imgLink = data.url;
-                    }
-                    add({
-                        title: document.querySelector("#title-post").value,
-                        img: imgLink || "",
-                        desc: document.querySelector("#desc-post").value,
-                    });
-                }
-                addProduct();
-            },
+                const { data } = await axios.post(CLOUDINARY_API, formData, {
+                    headers: {
+                        "Content-Type": "application/form-data",
+                    },
+                });
+                imgLink = data.url;
+            }
+            update({
+                id,
+                title: document.querySelector("#title-post").value,
+                img: imgLink || imgPreview.src,
+                desc: document.querySelector("#desc-post").value,
+            });
         });
     },
 };
-export default AdminAddNews;
+export default AdminEditNews;
